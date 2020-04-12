8c7258830ea04c67b5096f814c07d1e8cc841651
AhmadNurKhairi <18FTT1703@student.pb.edu.bn>
2020-04-12 18:02:44 +0800

    initial commit

---
+++ serviceWorker.js
@@ -0,0 +1,56 @@
+var APP_PREFIX = 'hikersblog'
+var VERSION = 'version_01'
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+  '/hikersblog/',
+  '/hikersblog/index.html',
+  '/hikersblog/about.html',
+  '/hikersblog/blog.html',
+  '/hikersblog/bshelang.html',
+  '/hikersblog/bsipatir.html',
+  '/hikersblog/contact.html',
+  '/hikersblog/journal.html,
+]
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
